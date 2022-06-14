c17be4cf7b58b319f4e7ab6c1a7a5ef4e285c05f
yc6 <17737266888@163.com>
2022-06-14 14:28:08 +0800

    ci(deps): add eslit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+        es6: true,
+    },
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+
+    rules: {},
+};
